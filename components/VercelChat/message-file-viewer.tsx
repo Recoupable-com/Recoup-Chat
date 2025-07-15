2544087d699fff2b52f94304870e84afe06931ab
veerbal1 <veerbalsingh1@gmail.com>
2025-07-15 16:45:08 +0530

    Integrate image preview functionality using PhotoProvider and PhotoView in MessageFileViewer component

--- components/VercelChat/message-file-viewer.tsx
+++ components/VercelChat/message-file-viewer.tsx
@@ -1,6 +1,7 @@
 import { Attachment } from "@ai-sdk/ui-utils";
 import { PDFIcon } from "./icons";
 import { FileIcon } from "lucide-react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
 
 const MessageFileViewer = ({
   experimentalAttachment,
@@ -11,17 +12,19 @@ const MessageFileViewer = ({
     return null;
   return (
     <div className="max-w-[17rem] flex gap-2 flex-wrap justify-end ml-auto">
+      <PhotoProvider>
       {experimentalAttachment?.map((attachment) => {
         if (attachment.contentType?.startsWith("image")) {
           return (
             <div key={attachment.url} className="w-16 h-16 rounded-xl overflow-hidden shadow-sm border">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                key={attachment.url}
+              <PhotoView key={attachment.url} src={attachment.url}>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
                 src={attachment.url}
                 alt={attachment.name}
                 className="w-full h-full object-cover"
               />
+              </PhotoView>
             </div>
           );
         }
@@ -34,6 +37,7 @@ const MessageFileViewer = ({
         }
         return <FileIcon key={attachment.url} />;
       })}
+      </PhotoProvider>
     </div>
   );
 };
