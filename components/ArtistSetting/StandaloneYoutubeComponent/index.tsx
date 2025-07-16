dd0d44e2a5f5c0c24bbded7abfaaf30166c11b8c
veerbal1 <veerbalsingh1@gmail.com>
2025-07-16 21:27:19 +0530

    Fixed label position for mobile device

--- components/ArtistSetting/StandaloneYoutubeComponent/index.tsx
+++ components/ArtistSetting/StandaloneYoutubeComponent/index.tsx
@@ -81,9 +81,9 @@ const StandaloneYoutubeComponent = ({
       )}
       {/* Graphic Arrow */}
       {data?.status === "invalid" && !isLoading && !hideArrow && (
-      <div className="absolute w-[10rem] right-[-8rem] top-[-2.5rem] opacity-[0.8] pointer-events-none">
+      <div className="absolute w-[7rem] right-[-5.7rem] top-[-1.5rem] md:w-[10rem] md:right-[-8rem] md:top-[-2.5rem] opacity-[0.8] pointer-events-none">
         <Image src={Arrow} alt="Youtube Arrow" className="w-full rotate-[10deg] scale-y-[0.8] opacity-[0.8]" />
-        <span className={cn("text-black absolute top-[3.5rem] left-[7rem] whitespace-nowrap rotate-[351deg] text-[1.4rem]", caveat.className)}>
+        <span className={cn("text-black absolute md:top-[3.5rem] md:left-[7rem] top-[2.5rem] left-[4.5rem] whitespace-nowrap rotate-[351deg] text-[1rem] md:text-[1.4rem]", caveat.className)}>
           Get Youtube Insights
           </span>
         </div>
