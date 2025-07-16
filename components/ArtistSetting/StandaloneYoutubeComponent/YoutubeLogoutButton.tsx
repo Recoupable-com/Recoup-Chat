7bfef14d4d4e5e50848ac686736ddfac01f02f7d
veerbal1 <veerbalsingh1@gmail.com>
2025-07-17 01:55:46 +0530

    Made token delete button same for both desktop and mobile

--- components/ArtistSetting/StandaloneYoutubeComponent/YoutubeLogoutButton.tsx
+++ components/ArtistSetting/StandaloneYoutubeComponent/YoutubeLogoutButton.tsx
@@ -31,16 +31,15 @@ const YoutubeLogoutButton = ({
   }
 
   return (
-    <div className="flex flex-col gap-1 cursor-pointer md:relative absolute bottom-0 -top-3 -right-1 md:top-0 md:right-0 md:justify-between md:pb-2">
+    <div className="flex flex-col gap-1 cursor-pointer absolute bottom-0 -top-3 -right-1 md:top-[-1rem]">
       <label className={"text-sm"}>&nbsp;</label>
       <Button
         size="icon"
-        className="md:w-9 md:h-9 w-4 h-4 bg-transparent text-red-500 md:bg-black md:text-white md:px-4 md:py-2 px-1 py-1 rounded-xl"
+        className="w-4 h-4 bg-transparent text-red-500 px-1 py-1 rounded-xl hover:bg-red-500 hover:text-white"
         onClick={() => logoutMutation.mutate()}
         disabled={logoutMutation.isPending}
       >
-        <LogOut className="hidden md:block" />
-        <XIcon className="md:hidden w-4 h-4" />
+        <XIcon className="w-4 h-4" />
       </Button>
     </div>
   );
