4b4764d3d8d760c377e872a7e43d72fe8b727809
Sweets Sweetman <sweetmantech@gmail.com>
2025-07-17 20:28:10 -0500

    balancing classnames

--- components/Segments/SegmentFanCircles.tsx
+++ components/Segments/SegmentFanCircles.tsx
@@ -19,8 +19,11 @@ const SegmentFanCircles = ({ fans }: SegmentFanCirclesProps) => {
           title={fan.username}
           style={{ marginLeft: index > 0 ? "-18px" : "0" }}
         >
-          <div className="w-[25px] h-[25px] md:w-[50px] md:h-[50px] rounded-full object-cover border-2 border-white hover:border-blue-400 transition-colors box-border">
-            <ImageWithFallback src={fan.avatar!} />
+          <div className="w-[25px] h-[25px] md:w-[50px] md:h-[50px]">
+            <ImageWithFallback
+              src={fan.avatar!}
+              className="rounded-full object-cover border-2 border-white hover:border-blue-400 transition-colors"
+            />
           </div>
           {fan.bio && (
             <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-1 w-32 p-1 bg-black/80 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none z-10">
