f309086c6f50640c3b5eb62b2a3de897c26c4f95
Sweets Sweetman <sweetmantech@gmail.com>
2025-07-22 10:23:10 -0500

    Home Page - include a link to your privacy policy.

--- components/Home/HomePage.tsx
+++ components/Home/HomePage.tsx
@@ -23,6 +23,16 @@ const HomePage = ({
   return (
     <div className="flex flex-col size-full items-center">
       <Chat id={id} initialMessages={initialMessages} />
+      {/* Legal links required by Google OAuth policy */}
+      <div className="mt-6 mb-4 text-xs text-gray-500">
+        <a href="/privacy" className="underline hover:text-gray-700">
+          Privacy Policy
+        </a>
+        <span className="mx-2">|</span>
+        <a href="/terms" className="underline hover:text-gray-700">
+          Terms of Service
+        </a>
+      </div>
     </div>
   );
 };
