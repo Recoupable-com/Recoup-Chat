2681b653dbcb0e2b01bc65e68fde3cfb0beb8381
Sweets Sweetman <sweetmantech@gmail.com>
2025-07-22 20:49:18 -0500

    remove unused provider imports

--- providers/Providers.tsx
+++ providers/Providers.tsx
@@ -1,14 +1,12 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ChatProvider } from "./ChatProvider";
 import PrivyProvider from "./PrivyProvider";
 import { UserProvider } from "./UserProvder";
 import { ArtistProvider } from "./ArtistProvider";
 import { ConversationsProvider } from "./ConversationsProvider";
 import { FunnelReportProvider } from "./FunnelReportProvider";
 import { PaymentProvider } from "./PaymentProvider";
-import { MessagesProvider } from "./MessagesProvider";
 import { SidebarExpansionProvider } from "./SidebarExpansionContext";
 import { MiniKitProvider } from "./MiniKitProvider";
 import WagmiProvider from "./WagmiProvider";
