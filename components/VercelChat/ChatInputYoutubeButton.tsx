62ac8d6b7fdffe7aa93b3f9ce46a7e989a7ff9db
veerbal1 <veerbalsingh1@gmail.com>
2025-07-28 16:57:09 +0530

    Remove unused import of useVercelChatContext in ChatInputYoutubeButton component

--- components/VercelChat/ChatInputYoutubeButton.tsx
+++ components/VercelChat/ChatInputYoutubeButton.tsx
@@ -3,11 +3,9 @@
 import { useArtistProvider } from "@/providers/ArtistProvider";
 import StandaloneYoutubeComponent from "../ArtistSetting/StandaloneYoutubeComponent";
 import ChatInputYoutubeButtonPopover from "../YouTube/ChatInputYoutubeButtonPopover";
-import { useVercelChatContext } from "@/providers/VercelChatProvider";
 
 export function ChatInputYoutubeButton() {
   const { selectedArtist } = useArtistProvider();
-  const { messages } = useVercelChatContext();
   
   return (
     <ChatInputYoutubeButtonPopover artistAccountId={selectedArtist?.account_id as string}>
