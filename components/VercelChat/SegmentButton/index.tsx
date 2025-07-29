9c47c607476f327eb73065330dd8c91049bb9d61
veerbal1 <veerbalsingh1@gmail.com>
2025-07-29 17:19:50 +0530

    Navigating to chat when user click on create segment button

--- components/VercelChat/SegmentButton/index.tsx
+++ components/VercelChat/SegmentButton/index.tsx
@@ -2,12 +2,30 @@ import { Tooltip } from "@/components/common/Tooltip";
 import MenuItemIcon from "@/components/MenuItemIcon";
 import { Button } from "@/components/ui/button";
 import GraphicArrow from "./GraphicArrow";
+import { useRouter } from "next/navigation";
+import { useVercelChatContext } from "@/providers/VercelChatProvider";
 
 const ChatInputSegmentCreationButton = () => {
+  const { push } = useRouter();
+  const { messages } = useVercelChatContext();
+
+  const handleSegmentClick = () => {
+    push(`/chat?q=${encodeURIComponent("Create a fan group")}`);
+  };
+
+  const shouldVisible = messages.length === 0;
+
+  if (!shouldVisible) return null;
+
   return (
     <div className="w-full relative flex items-center justify-center">
       <Tooltip content="Create Fan Groups">
-        <Button variant="outline" size="icon" className="rounded-full w-8 h-8">
+        <Button
+          variant="outline"
+          size="icon"
+          className="rounded-full w-8 h-8"
+          onClick={handleSegmentClick}
+        >
           <MenuItemIcon name="segments" />
           <span className="sr-only">Create Segment</span>
         </Button>
