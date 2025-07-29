df96181d4fbd8d9719729578bb2d2e244902d557
veerbal1 <veerbalsingh1@gmail.com>
2025-07-29 17:13:12 +0530

    Added tooltip and graphic arrow illustration

--- components/VercelChat/SegmentButton/index.tsx
+++ components/VercelChat/SegmentButton/index.tsx
@@ -1,13 +1,18 @@
+import { Tooltip } from "@/components/common/Tooltip";
 import MenuItemIcon from "@/components/MenuItemIcon";
 import { Button } from "@/components/ui/button";
+import GraphicArrow from "./GraphicArrow";
 
 const ChatInputSegmentCreationButton = () => {
   return (
     <div className="w-full relative flex items-center justify-center">
-      <Button variant="outline" size="icon" className="rounded-full w-8 h-8">
-        <MenuItemIcon name="segments" />
-        <span className="sr-only">Create Segment</span>
-      </Button>
+      <Tooltip content="Create Fan Groups">
+        <Button variant="outline" size="icon" className="rounded-full w-8 h-8">
+          <MenuItemIcon name="segments" />
+          <span className="sr-only">Create Segment</span>
+        </Button>
+      </Tooltip>
+      <GraphicArrow />
     </div>
   );
 };
