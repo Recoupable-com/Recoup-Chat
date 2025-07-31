0d1f3c1131b11a0a20dcb5415a059e2172432df5
Sweets Sweetman <sweetmantech@gmail.com>
2025-07-31 15:43:32 -0500

    add stopWhen param for all chat requests with default of 11 tool calls.

--- lib/chat/types.ts
+++ lib/chat/types.ts
@@ -1,6 +1,12 @@
 import { AnthropicProviderOptions } from "@ai-sdk/anthropic";
 import { GoogleGenerativeAIProviderOptions } from "@ai-sdk/google";
-import { ModelMessage, LanguageModel, UIMessage, ToolSet } from "ai";
+import {
+  ModelMessage,
+  LanguageModel,
+  UIMessage,
+  ToolSet,
+  StopCondition,
+} from "ai";
 
 export interface ChatRequest {
   messages: Array<UIMessage>;
@@ -16,6 +22,7 @@ export interface ChatConfig {
   messages: ModelMessage[];
   experimental_generateMessageId: () => string;
   tools: ToolSet;
+  stopWhen: StopCondition<ToolSet>;
   providerOptions?: {
     anthropic?: AnthropicProviderOptions;
     google?: GoogleGenerativeAIProviderOptions;
