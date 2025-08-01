6f028f49bc4a7180297ebeb0f1948982890b8d77
sweetman.eth <sweetmantech@gmail.com>
2025-08-01 16:37:17 -0300

    setupChatRequest - prepareStep - dev logging (#947)

--- lib/chat/types.ts
+++ lib/chat/types.ts
@@ -1,6 +1,13 @@
 import { AnthropicProviderOptions } from "@ai-sdk/anthropic";
 import { GoogleGenerativeAIProviderOptions } from "@ai-sdk/google";
-import { ModelMessage, LanguageModel, UIMessage, ToolSet, StopCondition } from "ai";
+import {
+  type ModelMessage,
+  type LanguageModel,
+  type UIMessage,
+  type ToolSet,
+  type StopCondition,
+  type PrepareStepFunction,
+} from "ai";
 
 export interface ChatRequest {
   messages: Array<UIMessage>;
@@ -16,9 +23,13 @@ export interface ChatConfig {
   messages: ModelMessage[];
   experimental_generateMessageId: () => string;
   tools: ToolSet;
+  prepareStep?: PrepareStepFunction;
   providerOptions?: {
     anthropic?: AnthropicProviderOptions;
     google?: GoogleGenerativeAIProviderOptions;
   };
-  stopWhen?: StopCondition<NoInfer<ToolSet>> | StopCondition<NoInfer<ToolSet>>[] | undefined
+  stopWhen?:
+    | StopCondition<NoInfer<ToolSet>>
+    | StopCondition<NoInfer<ToolSet>>[]
+    | undefined;
 }
