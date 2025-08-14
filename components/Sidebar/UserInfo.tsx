2e5125c84fe15629087151cdea7c6d82000a33e5
veerbal1 <veerbalsingh1@gmail.com>
2025-08-14 15:44:42 +0530

    Refactored User Account Button

--- components/Sidebar/UserInfo.tsx
+++ components/Sidebar/UserInfo.tsx
@@ -1,41 +1,19 @@
 import { useUserProvider } from "@/providers/UserProvder";
-import Image from "next/image";
 import Icon from "../Icon";
+import UserProfileButton from "./UserProfileButton";
 
 const UserInfo = ({
   toggleMenuExpanded,
 }: {
   toggleMenuExpanded: () => void;
 }) => {
-  const { email, toggleModal, userData } = useUserProvider();
+  const { email } = useUserProvider();
 
   return (
     <div
       className={`w-full flex gap-3 items-center ${email ? "justify-between" : "justify-end pr-2"}`}
     >
-      {userData && (
-        <button
-          className="flex gap-2 items-center flex-1 hover:bg-accent border border-transparent hover:border-grey rounded-xl py-1 pl-1"
-          type="button"
-          onClick={toggleModal}
-        >
-          <div className="relative w-6 h-6 md:w-8 md:h-8 rounded-md overflow-hidden">
-            <Image
-              src={userData?.image || "https://i.imgur.com/QCdc8Ai.jpg"}
-              layout="fill"
-              alt="not found icon"
-            />
-          </div>
-          <div>
-            <p className="text-xs md:text-sm truncate max-w-[190px]">
-              {userData?.name || email || userData?.wallet}
-            </p>
-            <p className="text-xs md:text-sm text-left">
-              {userData?.organization}
-            </p>
-          </div>
-        </button>
-      )}
+      <UserProfileButton />
       <button type="button" onClick={toggleMenuExpanded}>
         <Icon name="exit" />
       </button>
