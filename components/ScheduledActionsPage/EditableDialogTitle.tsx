eebb59582f22a22cf574dd80a791c63258d8db64
veerbal1 <veerbalsingh1@gmail.com>
2025-08-19 15:15:37 +0530

    Removed sonner

--- components/ScheduledActionsPage/EditableDialogTitle.tsx
+++ components/ScheduledActionsPage/EditableDialogTitle.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 import { Check, X, Pencil } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { toast } from "sonner";
+import { toast } from "react-toastify";
 
 interface EditableDialogTitleProps {
   title: string;
