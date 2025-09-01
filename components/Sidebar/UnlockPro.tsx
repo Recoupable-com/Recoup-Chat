04275089d38c390e5f1bc4eec71807f679406067
veerbal1 <veerbalsingh1@gmail.com>
2025-09-01 22:46:50 +0530

    Removed Unlock Pro Card

--- components/Sidebar/UnlockPro.tsx
+++ components/Sidebar/UnlockPro.tsx
@@ -1,13 +1,12 @@
 import { usePaymentProvider } from "@/providers/PaymentProvider";
 import UnlimitedCredits from "./UnlimitedCredits";
-import UnlockProCard from "./UnlockProCard";
 
 const UnlockPro = () => {
   const { subscriptionActive } = usePaymentProvider();
 
   return (
     <div className="-mt-px">
-      {subscriptionActive ? <UnlimitedCredits /> : <UnlockProCard />}
+      {subscriptionActive ? <UnlimitedCredits /> : null}
     </div>
   );
 };
