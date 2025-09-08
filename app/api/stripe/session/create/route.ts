b681b23cca352fdaf0a9cb01d81bb36013bb9af9
Sweets Sweetman <sweetmantech@gmail.com>
2025-09-08 18:15:46 -0500

    Add stripe typing.

--- app/api/stripe/session/create/route.ts
+++ app/api/stripe/session/create/route.ts
@@ -1,5 +1,6 @@
 import stripeClient from "@/lib/stripe/client";
 import { NextRequest } from "next/server";
+import Stripe from "stripe";
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
@@ -10,14 +11,13 @@ export async function POST(req: NextRequest) {
   const productName = body.productName;
   const totalPrice = body.totalPrice;
 
-  const priceData = {
+  const priceData: Stripe.Checkout.SessionCreateParams.LineItem.PriceData = {
     currency: "usd",
     unit_amount: isSubscription ? 2000 : totalPrice,
     product_data: {
       name: productName,
     },
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } as any;
+  };
 
   if (isSubscription)
     priceData.recurring = {
@@ -26,7 +26,7 @@ export async function POST(req: NextRequest) {
     };
 
   try {
-    const sessionData = {
+    const sessionData: Stripe.Checkout.SessionCreateParams = {
       success_url: successUrl as string,
       line_items: [
         {
@@ -37,12 +37,12 @@ export async function POST(req: NextRequest) {
       mode: isSubscription ? "subscription" : "payment",
       client_reference_id: referenceId,
       metadata,
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } as any;
-    if (isSubscription)
+    };
+    if (isSubscription) {
       sessionData.subscription_data = {
         metadata,
       };
+    }
     const session = await stripeClient.checkout.sessions.create(sessionData);
 
     return Response.json({ data: session }, { status: 200 });
