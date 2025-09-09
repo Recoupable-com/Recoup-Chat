75302e4005f657c78d9bdc6cb2bb00ed10c70559
Sweets Sweetman <sweetmantech@gmail.com>
2025-09-09 09:02:12 -0500

    createBillingPortalSession - new lib for single responsibility principle.

--- app/api/stripe/portal/create/route.ts
+++ app/api/stripe/portal/create/route.ts
@@ -1,5 +1,4 @@
-import stripeClient from "@/lib/stripe/client";
-import { getActiveSubscriptionDetails } from "@/lib/stripe/getActiveSubscriptionDetails";
+import createBillingPortalSession from "@/lib/stripe/createBillingPortalSession";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
@@ -8,22 +7,10 @@ export async function POST(req: NextRequest) {
   const returnUrl = body.returnUrl;
 
   try {
-    // Find the active subscription for this account
-    const activeSubscription = await getActiveSubscriptionDetails(accountId);
-
-    if (!activeSubscription) {
-      return Response.json(
-        { message: "No active subscription found for this account" },
-        { status: 404 }
-      );
-    }
-
-    // Create billing portal session using the customer from the subscription
-    const portalSession = await stripeClient.billingPortal.sessions.create({
-      customer: activeSubscription.customer as string,
-      return_url: returnUrl,
-    });
-
+    const portalSession = await createBillingPortalSession(
+      accountId,
+      returnUrl
+    );
     return Response.json({ data: portalSession }, { status: 200 });
   } catch (error) {
     console.error(error);
