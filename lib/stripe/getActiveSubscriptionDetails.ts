de0f6e952132c963e2152439940c2756302d6318
Sweets Sweetman <sweetmantech@gmail.com>
2025-09-09 09:11:25 -0500

    DRY - update /api/stripe/subscription/search to call getActiveSubscriptionDetails

--- lib/stripe/getActiveSubscriptionDetails.ts
+++ lib/stripe/getActiveSubscriptionDetails.ts
@@ -1,20 +1,8 @@
-import stripeClient from "./client";
-import Stripe from "stripe";
+import { getActiveSubscriptions } from "./getActiveSubscriptions";
 
 export const getActiveSubscriptionDetails = async (accountId: string) => {
   try {
-    const subscriptions = await stripeClient.subscriptions.list({
-      limit: 100,
-      current_period_end: {
-        gt: parseInt(Number(Date.now() / 1000).toFixed(0), 10),
-      },
-    });
-
-    const activeSubscriptions = subscriptions?.data?.filter(
-      (subscription: Stripe.Subscription) =>
-        subscription.metadata?.accountId === accountId
-    );
-
+    const activeSubscriptions = await getActiveSubscriptions(accountId);
     return activeSubscriptions.length > 0 ? activeSubscriptions[0] : null;
   } catch (error) {
     console.error("Error fetching subscription:", error);
