c586771253242a272a02162c88eae1d396d9da2e
veerbal1 <veerbalsingh1@gmail.com>
2025-09-09 15:02:08 +0530

    Fixed height issue

--- components/VercelChat/ChatInput.tsx
+++ components/VercelChat/ChatInput.tsx
@@ -146,18 +146,26 @@ export function ChatInput({
                 border: "none",
                 outline: "none",
                 background: "transparent",
+                width: "100%",
               },
               "&multiLine": {
                 highlighter: {
                   padding: "12px 20px",
                   fontSize: 14,
                   lineHeight: 1.6,
+                  maxHeight: 180,
+                  overflow: "hidden",
                 },
                 input: {
                   padding: "12px 20px",
                   outline: "none",
                   fontSize: 14,
                   lineHeight: 1.6,
+                  minHeight: 44,
+                  maxHeight: 180,
+                  overflowY: "auto",
+                  resize: "none",
+                  boxSizing: "border-box",
                 },
               },
               "&singleLine": {
