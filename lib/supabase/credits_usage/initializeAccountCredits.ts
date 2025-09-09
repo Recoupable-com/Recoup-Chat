51f4a6c8f253e9c1ed8f407b02480ff0e4d8c66b
Sweets Sweetman <sweetmantech@gmail.com>
2025-09-09 10:29:22 -0500

    New Sign Up - 333 credits instead of 1 credit

--- lib/supabase/credits_usage/initializeAccountCredits.ts
+++ lib/supabase/credits_usage/initializeAccountCredits.ts
@@ -1,8 +1,9 @@
+import { DEFAULT_CREDITS } from "@/lib/consts";
 import supabase from "../serverClient";
 
 export const initializeAccountCredits = async (accountId: string) => {
   await supabase.from("credits_usage").insert({
     account_id: accountId,
-    remaining_credits: initialCredits,
+    remaining_credits: DEFAULT_CREDITS,
   });
 };
