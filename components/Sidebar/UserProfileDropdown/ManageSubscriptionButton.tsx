e3072ab004a9f07c0ab21dc2cd76144d4d6a1261
Sweets Sweetman <sweetmantech@gmail.com>
2025-09-09 09:31:31 -0500

    ManageSubscriptionButton - onClick - if !subscribed - open Pro subscription page

--- components/Sidebar/UserProfileDropdown/ManageSubscriptionButton.tsx
+++ components/Sidebar/UserProfileDropdown/ManageSubscriptionButton.tsx
@@ -1,24 +1,28 @@
 import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
 import { IconCreditCard } from "@tabler/icons-react";
 import { useUserProvider } from "@/providers/UserProvder";
+import { usePaymentProvider } from "@/providers/PaymentProvider";
 import createClientPortalSession from "@/lib/stripe/createClientPortalSession";
+import createClientCheckoutSession from "@/lib/stripe/createClientCheckoutSession";
 
 const ManageSubscriptionButton = () => {
   const { userData } = useUserProvider();
+  const { subscriptionActive } = usePaymentProvider();
 
-  const handleManageSubscription = () => {
-    if (userData?.account_id) {
+  const handleClick = () => {
+    if (!userData?.account_id) return;
+
+    if (subscriptionActive) {
       createClientPortalSession(userData.account_id);
+      return;
     }
+    createClientCheckoutSession(userData.account_id);
   };
 
   return (
-    <DropdownMenuItem
-      onClick={handleManageSubscription}
-      className="cursor-pointer"
-    >
+    <DropdownMenuItem onClick={handleClick} className="cursor-pointer">
       <IconCreditCard />
-      Manage Subscription
+      {subscriptionActive ? "Manage Subscription" : "Subscribe"}
     </DropdownMenuItem>
   );
 };
