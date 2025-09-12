c2eb3f0c7a057764bebbbeac51b95eaf449e7734
sweetman.eth <sweetmantech@gmail.com>
2025-09-12 19:09:51 -0300

    Sweetmantech/myc 2887 account emails enforce unique email column (#1132)
    * usePayment - isEnterprise lib to set subscription active state
    
    * add recoupable.com
    
    * account_emails - enforce unique email column
    
    * remove incorrect created_at column reference

--- lib/stripe/client.tsx
+++ lib/stripe/client.tsx
@@ -1,6 +1,5 @@
 import Stripe from "stripe";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const stripeClient = new Stripe(process.env.STRIPE_SK as string) as any;
+const stripeClient = new Stripe(process.env.STRIPE_SK as string);
 
 export default stripeClient;
