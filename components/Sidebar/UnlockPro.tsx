5b97b4aee061da7b91ce9cf63fbbf9e0cd41606f
Sweets Sweetman <sweetmantech@gmail.com>
2025-09-17 09:01:47 -0500

    pro - do not flash the UpgradeCard

--- components/Sidebar/UnlockPro.tsx
+++ components/Sidebar/UnlockPro.tsx
@@ -3,7 +3,9 @@ import RecoupablePro from "./RecoupablePro";
 import UnlockProCard from "./UnlockProCard";
 
 const UnlockPro = () => {
-  const { subscriptionActive } = usePaymentProvider();
+  const { subscriptionActive, isLoadingCredits } = usePaymentProvider();
+
+  if (isLoadingCredits) return null;
 
   return (
     <div className="-mt-px">
