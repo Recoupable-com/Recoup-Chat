408ce97951f8ace4e6f7af46c6e829db8a99830e
sweetman.eth <sweetmantech@gmail.com>
2025-09-25 19:56:30 -0300

    Evals - how many total followers does Gliiico have on Spotify (#1237)
    * Evals - how many total followers does Gliiico have on Spotify
    
    * remove redundant dev comments.
    
    * Single Responsibility Principle - getSpotifyFollowersData and getSpotifyFollowersExpected.
    
    * remove redundant dev comments.
    
    * remove redundant dev comment.
    
    * remove redundant dev comment.

---
+++ lib/evals/getSpotifyFollowersData.ts
@@ -0,0 +1,31 @@
+import getSpotifyFollowersExpected from "@/lib/evals/getSpotifyFollowersExpected";
+
+const getSpotifyFollowersData = async () => {
+  const artists = [
+    "Gliiico",
+    "Mac Miller",
+    "Wiz Khalifa",
+    "Mod Sun",
+    "Julius Black",
+  ];
+
+  const testCases = await Promise.all(
+    artists.map(async (artist) => {
+      const { expected } = await getSpotifyFollowersExpected(artist);
+      return {
+        input: `how many total followers does ${artist} have on Spotify`,
+        expected,
+        metadata: {
+          artist,
+          platform: "Spotify",
+          expected_tool_usage: true,
+          data_type: "spotify_followers",
+        },
+      };
+    })
+  );
+
+  return testCases;
+};
+
+export default getSpotifyFollowersData;
