40cf89caddf1fc689cbf06ea604bbdb69e111928
Sweets Sweetman <sweetmantech@gmail.com>
2025-10-08 09:54:15 -0500

    Tools - catalog - insert_catalog_songs

--- lib/tools/catalogs/index.ts
+++ lib/tools/catalogs/index.ts
@@ -1,9 +1,11 @@
 import getCatalogsTool from "./getCatalogs";
 import getCatalogSongsTool from "./getCatalogSongs";
+import insertCatalogSongsTool from "./insertCatalogSongs";
 
 const catalogTools = {
   select_catalogs: getCatalogsTool,
   select_catalog_songs: getCatalogSongsTool,
+  insert_catalog_songs: insertCatalogSongsTool,
 };
 
 export default catalogTools;
