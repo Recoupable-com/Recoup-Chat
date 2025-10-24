165291b6336d217b63bd71fd08a49a5e7e862645
sweetman.eth <sweetmantech@gmail.com>
2025-10-24 11:47:22 -0300

    Catalog Page - login popup if not logged in (#1331)
    * Catalog Page - login popup if not logged in
    
    * adjust usage of use client to components where hooks are imported

--- components/Catalog/CatalogPage.tsx
+++ components/Catalog/CatalogPage.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import { useAutoLogin } from "@/hooks/useAutoLogin";
+
 const CatalogPage = () => {
+  useAutoLogin();
+
   return (
     <div className="max-w-screen min-h-screen p-4">
       <h1 className="text-lg md:text-xl font-bold pb-4">Catalog</h1>
