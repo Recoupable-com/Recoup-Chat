ba938432dd0fea0612e81081dd858b07b34c9f23
Sweets Sweetman <sweetmantech@gmail.com>
2025-10-24 09:39:46 -0500

    Catalog Page - login popup if not logged in

--- components/Catalog/CatalogPage.tsx
+++ components/Catalog/CatalogPage.tsx
@@ -1,4 +1,8 @@
+import { useAutoLogin } from "@/hooks/useAutoLogin";
+
 const CatalogPage = () => {
+  useAutoLogin();
+
   return (
     <div className="max-w-screen min-h-screen p-4">
       <h1 className="text-lg md:text-xl font-bold pb-4">Catalog</h1>
