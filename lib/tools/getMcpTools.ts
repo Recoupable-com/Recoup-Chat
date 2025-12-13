8cd47884bd7ac1ec97e5323f8c74d147cb33e958
Sweets Sweetman <sweetmantech@gmail.com>
2025-12-13 16:46:19 -0500

    get_spotify_artist_albums

--- lib/tools/getMcpTools.ts
+++ lib/tools/getMcpTools.ts
@@ -9,7 +9,6 @@ import searchGoogleImages from "./searchGoogleImages";
 import generateMermaidDiagram from "./generateMermaidDiagram";
 import createArtist from "./createArtist";
 import deleteArtist from "./deleteArtist";
-import getSpotifyArtistAlbums from "./getSpotifyArtistAlbums";
 import getSpotifyAlbum from "./getSpotifyAlbum";
 import updateAccountInfo from "./updateAccountInfo";
 import updateArtistSocialsTool from "./updateArtistSocials";
@@ -45,7 +44,6 @@ export function getMcpTools(): ToolSet {
     generate_mermaid_diagram: generateMermaidDiagram,
     create_new_artist: createArtist,
     delete_artist: deleteArtist,
-    get_spotify_artist_albums: getSpotifyArtistAlbums,
     get_spotify_album: getSpotifyAlbum,
     update_account_info: updateAccountInfo,
     update_artist_socials: updateArtistSocialsTool,
